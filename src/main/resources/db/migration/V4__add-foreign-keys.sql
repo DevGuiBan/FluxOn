5f4141de0a81193bfa4f0e0fe7958942e9ba8fc3
Guilherme Bandeira <107276182+DevGuiBan@users.noreply.github.com>
2025-09-21 23:58:36 -0300

    Iniciando WorkSchedule

--- src/main/resources/db/migration/V4__add-foreign-keys.sql
+++ src/main/resources/db/migration/V4__add-foreign-keys.sql
@@ -1,11 +1,12 @@
+-- User Specs -> Users (One-to-One)
 ALTER TABLE user_specs
 ADD CONSTRAINT fk_user_specs_user
 FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE;
-
+-- User Specs -> Responsibilities (Many-to-One)
 ALTER TABLE user_specs
 ADD CONSTRAINT fk_user_specs_responsibility
 FOREIGN KEY (responsibility_id) REFERENCES responsibilities(id) ON DELETE SET NULL;
-
+-- Users -> User Specs (One-to-One)
 ALTER TABLE users
 ADD CONSTRAINT fk_users_user_spec
 FOREIGN KEY (user_spec_id) REFERENCES user_specs(id) ON DELETE SET NULL;
