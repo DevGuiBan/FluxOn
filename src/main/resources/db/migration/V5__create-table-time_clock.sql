2f3ebedf9805ff1fa293fc7ed65eb2309bf97ce8
DevGuiBan <guibandeira.dev@gmail.com>
2025-10-11 13:49:15 -0300

    feat: update time clock table schema to include turn and rename clock_in to clock

--- src/main/resources/db/migration/V5__create-table-time_clock.sql
+++ src/main/resources/db/migration/V5__create-table-time_clock.sql
@@ -1,11 +1,11 @@
 CREATE TABLE time_clock (
     id UUID PRIMARY KEY DEFAULT GEN_RANDOM_UUID(),
     user_id UUID NOT NULL,
-    clock_in TIME,
+    clock TIME,
     clock_out TIME,
     date DATE NOT NULL,
+    turn VARCHAR(50) NOT NULL,
     attendance_status VARCHAR(50) NOT NULL,
-    justification VARCHAR(255),
 
     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
